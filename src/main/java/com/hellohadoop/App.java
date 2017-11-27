bc605f3ab9d3d9e3eec800bcfa1ac980f5b87eea
Wei.Liu <xiruanliuwei@163.com>
2017-11-27 23:40:00 +0800

    Add MaxTemperature of functions map and reduce

--- src/main/java/com/hellohadoop/App.java
+++ src/main/java/com/hellohadoop/App.java
@@ -1,13 +1,43 @@
 package com.hellohadoop;
 
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import com.sun.jersey.core.impl.provider.entity.XMLJAXBElementProvider.Text;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) throws Exception
     {
         System.out.println( "Hello World!" );
+        
+        if(args.length != 2)
+        {
+        		System.err.println("Usage: App <input path> <output path>");
+        		System.exit(-1);
+        }
+        
+        Job job = new Job();
+        // Set the Jar by finding where a given class came from.
+        job.setJarByClass(App.class);
+        job.setJobName("Max Temperature");
+        
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+        
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
